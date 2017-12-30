1783ee80f518507884c4a77549edc0beaf1ae9ab
yuyabu <yuyabushita@gmail.com>
2017-12-31 06:22:15 +0900

    章が変わるので一旦コミット
    DollarとFrancにあるtimeメソッドのシグニチャを合わせた。
    メソッド宣言を親クラスに移した。
    コンパイルを通すためのabstract修飾子の使い方がなかなか豪快だと思った。

--- TDDTranscription/src/money/Franc.java
+++ TDDTranscription/src/money/Franc.java
@@ -5,7 +5,7 @@ public class Franc extends Money{
 	Franc(int amount){
 		this.amount = amount;
 	}
-	Franc times(int multiplier) {
+	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
 		
 	}
--- TDDTranscription/src/money/Money.java
+++ TDDTranscription/src/money/Money.java
@@ -1,11 +1,19 @@
 package money;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount
 				&& getClass().equals(money.getClass());
 	}
+	static Money dollar(int amount) {
+		
+		return new Dollar(amount);
+	}
+	static Money franc(int amount) {
+		return new Franc(amount);
+	}
+	abstract Money times(int multiplier);
 
 }
