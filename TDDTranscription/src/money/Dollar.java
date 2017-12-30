37a12491b29c6586f23f76f6e6435c9869c5f0c8
yuyabu <yuyabushita@gmail.com>
2017-12-30 18:29:16 +0900

    equalsを実装した

--- TDDTranscription/src/money/Dollar.java
+++ TDDTranscription/src/money/Dollar.java
@@ -9,7 +9,8 @@ public class Dollar {
 		return new Dollar(amount* multiplier);
 	}
 	public boolean equals(Object object) {
-		return true;
+		Dollar dollar = (Dollar) object;
+		return amount == dollar.amount;
 	}
 
 
--- TDDTranscription/src/money/MoneyTest.java
+++ TDDTranscription/src/money/MoneyTest.java
@@ -19,6 +19,6 @@ class MoneyTest {
 	@Test
 	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
-		
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 }
