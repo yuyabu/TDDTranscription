a2b1efd8ab70a1f7c7b191388bdda86b0c2d44af
yuyabu <yuyabushita@gmail.com>
2017-12-31 18:53:30 +0900

    Bankの仮実装

--- TDDTranscription/src/money/Money.java
+++ TDDTranscription/src/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+public class Money implements Expression{
 	protected int amount;
 	protected String currency;
 	
@@ -29,5 +29,8 @@ public class Money {
 	}
 	public String toString() {
 		return amount + "" + currency;
+	}
+	Expression plus(Money added) {
+		return new Money(this.amount + added.amount,currency);
 	}
 }
