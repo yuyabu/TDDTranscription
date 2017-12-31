cb602c32ee400cd4e4530270515701761bcaa229
yuyabu <yuyabushita@gmail.com>
2017-12-31 09:22:23 +0900

    Moneyのサブクラスを全て削除した

--- TDDTranscription/src/money/MoneyTest.java
+++ TDDTranscription/src/money/MoneyTest.java
@@ -17,8 +17,6 @@ class MoneyTest {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
 	}
 	@Test
@@ -32,8 +30,5 @@ class MoneyTest {
 		assertEquals("USD",Money.dollar(1).currency());
 		assertEquals("CHF",Money.franc(1).currency());
 	}
-	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
-	}
+	
 }
