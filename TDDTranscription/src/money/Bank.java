546926a00d1852f6a5d6bd63a809a87066450d89
yuyabu <yuyabushita@gmail.com>
2018-01-01 16:10:34 +0900

    為替計算をMoneyクラスではなくBankクラスで行えるように変更

--- TDDTranscription/src/money/Bank.java
+++ TDDTranscription/src/money/Bank.java
@@ -10,10 +10,12 @@ class Bank {
 	 */
 	Money reduce(Expression source,String to) {
 		
-		return source.reduce(to);
+		return source.reduce(this,to);
 	}
-	void addRate(String from,String to,int rate) {
-		
+	void addRate(String from,String to,int rate) {	
 	}
+	int rate(String from,String to) {
+		return(from.equals("CHF")&& to.equals("USD")) ? 2:1;
 
+	}
 }
