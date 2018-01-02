d2dfa8e345bf3b6341cd0814d269149d730b397e
yuyabu <yuyabushita@gmail.com>
2018-01-02 18:17:30 +0900

    Expression.timesの実装

--- TDDTranscription/src/money/Money.java
+++ TDDTranscription/src/money/Money.java
@@ -21,7 +21,7 @@ public class Money implements Expression{
 	static Money franc(int amount) {
 		return new Money(amount,"CHF");
 	}
-	 Money times(int multiplier) {
+	 public Money times(int multiplier) {
 		 return new Money(amount * multiplier,currency);
 	 };
 	String currency() {
